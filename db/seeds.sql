8b1524b16c0fc2e2affca190890a1bb221b3d4fd
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-28 18:12:15 -0500

    seeds.sql fully seeds the database from what i can see accuratly

--- db/seeds.sql
+++ db/seeds.sql
@@ -21,21 +21,21 @@ VALUES  ("Accounting Manager", 130000.00, 1),
         ("Human Resources Specialists ", 92853.37, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Robert", "Miller", 1),
+VALUES ("Robert", "Miller", 1, 1),
        ("Ruben", "Stillwell", 2, 1),
        ("Bobby", "Hernandez", 2, 1),
        ("William", "Jones", 3, 1),
 
-       ("Mary", "Schroeder", 4),
+       ("Mary", "Schroeder", 4, 4),
        ("Carl", "Booth", 5, 4),
        ("Hillary", "Spillers", 5, 4),
 
-       ("Jesus", "Ellis", 6),
+       ("Jesus", "Ellis", 6, 7),
        ("Jeff", "Corder", 7, 7),
        ("James", "Bahe", 8, 7),
        ("Carol", "Macdonald", 8, 7),
        ("Harry", "Warren", 9, 7),
 
-       ("Alejandro", "Bryner", 10),
+       ("Alejandro", "Bryner", 10, 12),
        ("Martin", "Tucker", 11, 12),
        ("Francisca", "Updike", 11, 12);
