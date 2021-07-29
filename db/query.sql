fd19aad8ec6b66a6c705987ba7ca45d4b3b63249
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-28 19:40:46 -0500

    query.sql creates the basic queried tables when asking for all of a certain scenario

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,14 @@
-SELECT * FROM company_db.department;
+/* department names and department ids */
+SELECT * FROM company_db.departments;
 
-SELECT * FROM company_db.role;
+/* job title, role id, the department that role belongs to, and the salary for that role */
+SELECT roles.id, roles.title, roles.salary, departments.department_name
+FROM roles
+INNER JOIN departments ON roles.department_id = departments.id;
 
-SELECT * FROM company_db.employee;
+/* including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to */
+SELECT employees.id as employee_id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.department_name, CONCAT(managers.first_name, " ", managers.last_name) AS manager_name
+FROM roles
+INNER JOIN employees ON roles.id = employees.role_id
+LEFT JOIN departments ON roles.department_id = departments.id
+LEFT JOIN employees managers ON employees.manager_id = managers.id
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO department (name)
+INSERT INTO departments (department_name)
 VALUES ("Accounting"),
        ("Sales"),
        ("Information Technology"),
        ("Human Resources");
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES  ("Accounting Manager", 130000.00, 1),
         ("Accountant", 97500.00, 1),
         ("Assistant Controller", 73125.80, 1),
@@ -20,22 +20,22 @@ VALUES  ("Accounting Manager", 130000.00, 1),
         ("Human Resources Managers ", 103170.41, 4),
         ("Human Resources Specialists ", 92853.37, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Robert", "Miller", 1, 1),
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Robert", "Miller", 1, null),
        ("Ruben", "Stillwell", 2, 1),
        ("Bobby", "Hernandez", 2, 1),
        ("William", "Jones", 3, 1),
 
-       ("Mary", "Schroeder", 4, 4),
-       ("Carl", "Booth", 5, 4),
-       ("Hillary", "Spillers", 5, 4),
+       ("Mary", "Schroeder", 4, null),
+       ("Carl", "Booth", 5, 5),
+       ("Hillary", "Spillers", 5, 5),
 
-       ("Jesus", "Ellis", 6, 7),
-       ("Jeff", "Corder", 7, 7),
-       ("James", "Bahe", 8, 7),
-       ("Carol", "Macdonald", 8, 7),
-       ("Harry", "Warren", 9, 7),
+       ("Jesus", "Ellis", 6, null),
+       ("Jeff", "Corder", 7, 8),
+       ("James", "Bahe", 8, 8),
+       ("Carol", "Macdonald", 8, 8),
+       ("Harry", "Warren", 9, 8),
 
-       ("Alejandro", "Bryner", 10, 12),
-       ("Martin", "Tucker", 11, 12),
-       ("Francisca", "Updike", 11, 12);
+       ("Alejandro", "Bryner", 10, null),
+       ("Martin", "Tucker", 11, 13),
+       ("Francisca", "Updike", 11, 13);
