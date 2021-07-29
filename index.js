581d61e73f92476a38827f1a3b5825e86656fe0e
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-28 20:52:39 -0500

    created a object to support many options later on

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const inquirer = require('inquirer');
 const queries = require('./lib/queries.js')
 
-const questionsArray = [
-    'all departments',
-    'view all roles',
-    'view all employees',
-    'add a department',
-    'add a role',
-    'add an employee',
-    'and update an employee role'
-]
-
+const Questions = {
+    main: [
+        'all departments',
+        'view all roles',
+        'view all employees',
+        'Quit',
+    ],
+    departments: [
+        'view departments',
+        'add a department',
+    ],
+    roles: [
+        'view roles',
+        'add a role',
+        'update a role',
+    ],
+    employees: [
+        'view employees',
+        'add an employees',
+    ]
+}
 
 //!view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 function init() {
@@ -22,33 +33,41 @@ function questions() {
     {
         type: 'list',
         message: 'Do somthin',
-        name: 'role',
-        choices: questionsArray
+        name: 'choice',
+        choices: Questions.main
     }
     ]).then((value) => {
         console.log(value)
-        switch(value.role){
-            case questionsArray[0]:
+        switch(value.choice){
+            case Questions.main[0]:
                 queries.selectDepartmentTable();
                 break
-            case questionsArray[1]:
+            case Questions.main[1]:
                 queries.selectRolesTable();
                 break
-            case questionsArray[2]:
+            case Questions.main[2]:
                 queries.selectEmployeesTable();
                 break
-            case questionsArray[3]:
+            case Questions.main[3]:
+                //! function to get information about these inserts
+                queries.addEmployeesTable();
                 break
-            case questionsArray[4]:
+            case Questions.main[4]:
+                //! function to get information about these inserts
+                queries.addRolesTable();
                 break
-            case questionsArray[5]:
-                break
-            case questionsArray[6]:
+            case Questions.main[5]:
+                //! function to get information about these inserts
+                queries.updateEmployeesTable();
                 break
             default:
-                console.log("error in switch case index.js", value.name, questionsArray[0])
+                console.log("error in switch case index.js", value.name);
         }
-        questions();
+        if(value.choice != 'Quit'){
+            console.clear();
+            questions();
+        }
+        process.abort();
     })
 }
 
